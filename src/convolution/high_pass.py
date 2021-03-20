1c85f94c3ec6ff74bd4fa091494b9efd9b63ef8e
JWLee89 <ljay189@gmail.com>
2021-03-20 18:14:33 +0900

    Updated README.md and added some start-up incremental code to high_pass.py

--- src/convolution/high_pass.py
+++ src/convolution/high_pass.py
@@ -8,7 +8,7 @@ Goal of high-pass filters is to
 """
 
 
-def high_pass_filter(image):
+def visualize_high_pass_filter(image):
     # Good practice to create deep copy of image before working on it
     img_copy = np.copy(image)
 
@@ -17,7 +17,28 @@ def high_pass_filter(image):
     # since it captures intensity information compactly
     # Why? because gray-scale images from 0 - 255 represent the amount of light
     # in a given scale I.E. whether it is light (255 - white) or dark (0 - black)
-    img_grayscale = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    img_grayscale = cv2.cvtColor(img_copy, cv2.COLOR_RGB2GRAY)
+
+    # High pass filters should add to zero.
+    # For example, like the 3x3 filter below
+    high_pass_kernel = np.array([
+        [0, -1, 0],
+        [-1, 4, 1],
+        [0, -1, 0]
+    ])
+
+    # Convolve the image using the high-pass filter
+    convolved_image = cv2.filter2D(img_grayscale, -1, high_pass_kernel)
+
+    # let's compare original image with convolved image
+    fig, (left_axis, right_axis) = plt.subplots(1, 2, figsize=(10, 5))
+
+    left_axis.set_title("Original image")
+    left_axis.imshow(img_grayscale)
+
+    right_axis.set_title("Convolved image")
+    right_axis.imgshow(convolved_image)
+    fig.show()
 
 
 if __name__ == "__main__":
@@ -25,5 +46,5 @@ if __name__ == "__main__":
     # By default, cv2 images are read as BGR instead of RBG
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # Apply high pass filter
-    new_image = high_pass_filter(image)
+    # Visualize high pass filter
+    visualize_high_pass_filter(image)
