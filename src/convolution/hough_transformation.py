a2105c388abfe3ef35433c56850322d442aa5690
JWLee89 <ljay189@gmail.com>
2021-03-29 16:51:09 +0900

    Added skeleton code for encoder decoder

---
+++ src/convolution/hough_transformation.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+"""
+The hough transformation projects a line in
+the image space onto a 1-dimensional line known
+as the Hough space. 
+In other words, it maps a line on a 2d image space
+onto a point in the hough space, represented by the coordinates of a 2d vector
+"""
+
+if __name__ == "__main__":
+    pass
---
+++ src/image_captioning/encoder_decoder/run.py
@@ -0,0 +1,45 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import argparse
+from torchvision.datasets.coco import CocoCaptions
+from torchvision import transforms
+
+
+def get_coco(data_path):
+    """
+
+    Args:
+        data_path ():
+
+    Returns:
+
+    """
+    annotation_file_path = data_path + '/captions_val2014_fakecap_results.json'
+    captions = CocoCaptions(root = data_path,
+                            annFile=annotation_file_path,
+                            transform=transforms.ToTensor())
+
+    print('Number of samples: ', len(captions))
+    img, target = captions[3]  # load 4th sample
+
+    print("Image Size: ", img.size())
+    print(target)
+
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--batch_size', type=int, default=64, help='Size of mini-batch')
+    parser.add_argument('-d', '--data_path', type=str, default= "../../../data")
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = get_args()
+
+    # TODO: set up dataloader, etc.
+    yee = get_coco(args.data_path)
+
+
+if __name__ == "__main__":
+    main()
